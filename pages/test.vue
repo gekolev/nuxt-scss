90dbcd88cf2e305eba98e6f3fe7403eb83dbd36d
George Kolev <gogo1520180@gmail.com>
2022-09-04 13:01:22 +0300

    init

---
+++ pages/test.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>hello world</h1>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss">
+@import "~assets/scss/variables.scss";
+
+h1{
+    color: $primary-color;
+}
+</style>
